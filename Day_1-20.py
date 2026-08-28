c4f87c4310cf6a94bfd8ebc202fa1354c7f6d054
Helios1718933 <Helios1718933@users.noreply.github.com>
2026-08-28 09:34:08 +0800

    day_15 had finished

--- Day_1-20.py
+++ Day_1-20.py
@@ -889,9 +889,76 @@ def captcha_generate(*,len = 4):
     print(ALL_CHARS)
     return ''.join(random.choices(ALL_CHARS, k=len)) 
 
+def is_prime(num: int) -> bool:
+#不需要中间态判断，只要遇到可以整除的就直接输出；
+    for i in range(2,int((num ** 0.5)) + 1):   
+    #TypeError: 'float' object cannot be interpreted as an integer
+        if num % i == 0:
+            return False
+    return True
+
+#----------------
+def lcm(x: int, y: int) -> int:
+    """求最小公倍数"""
+    return x * y // gcd(x, y)
 
 
-1231231231231231
+def gcd(x: int, y: int) -> int:
+    """求最大公约数"""
+    while y % x != 0:
+        x, y = y % x, x
+    return x
+
+#函数之间的相互调用
+#++++++++++++++++
+
+def ptp(data):
+    """极差（全距）"""
+    return max(data) - min(data)
+
+
+def mean(data):
+    """算术平均"""
+    return sum(data) / len(data)
+
+
+def median_self(data):    #对于自己手写的案例函数，函数名加“_self”
+    """中位数"""
+    temp,data_len = sorted(data), len(data)
+    if data_len % 2 == 0:
+        return mean(temp[data_len // 2 - 1:data_len // 2 +1 ])  #4/2 = 2.0 not integer
+    else:
+        return temp[data_len // 2]
+   
+
+
+def var(data, ddof=1):
+    """方差"""
+    x_bar = mean(data)
+    temp = [(num - x_bar) ** 2 for num in data]
+    return sum(temp) / (len(temp) - ddof)
+
+
+def std(data, ddof=1):
+    """标准差"""
+    return var(data, ddof) ** 0.5
+
+
+def cv(data, ddof=1):
+    """变异系数"""
+    return std(data, ddof) / mean(data)
+
+
+def describe(data):
+    """输出描述性统计信息"""
+    print(f'均值: {mean(data)}')
+    print(f'中位数: {median(data)}')
+    print(f'极差: {ptp(data)}')
+    print(f'方差: {var(data)}')
+    print(f'标准差: {std(data)}')
+    print(f'变异系数: {cv(data)}')
+
+
 
 if __name__ == '__main__':
-    print(captcha_generate(len = 5))
+    print(median_self([1,2,3,4,9]))
